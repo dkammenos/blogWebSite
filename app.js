da18952143e6250506f9dc82b2945a99f83f0fb9
ΔΙΟΝΥΣΙΟΣ ΚΑΜΜΕΝΟΣ <std080228@ac.eap.gr>
2022-12-05 13:23:12 +0200

    Neo app

--- app.js
+++ app.js
@@ -12,13 +12,12 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
 app.get('/', (req, res) => {
   res.render('home', {foo: 'FOO'});
 });
-
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
 
 
 
