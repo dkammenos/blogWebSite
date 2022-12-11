535bb13681c828662b98e34462e2c5ac396ab844
Dionysios Kammenos <kammenos@hotmail.com>
2022-12-11 02:04:43 +0200

    Section 23 challenges completed

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+// Load the full build.
+const _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,12 +18,31 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 let Posts=[];
+
+
   
   
 app.get('/', (req, res) => {
   res.render('home', {message: homeStartingContent,posts:Posts });
-  //challenge 11
-  console.log(Posts);
+  //challenge 11   console.log(Posts);
+});
+
+app.get('/posts/:post', (req, res) => {
+
+   console.log(req.params); // challenge 16
+  let reqparam = _.lowerCase(req.params.post);
+ // console.log(_.lowerCase(reqparam)); 
+  Posts.forEach(post => {  
+    if (reqparam === _.lowerCase(post.title)) {
+      res.render('post', {posttitle:post.title, postbody: post.postbody.slice(12) });
+      console.log("Match Found");
+    } else {
+      console.log("Post not found");
+    }
+    
+  })  ;
+  res.redirect("/");
+ 
 });
 
 app.get('/about', (req, res) => {
